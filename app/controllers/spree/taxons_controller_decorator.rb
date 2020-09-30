ae3b0180bd86c36222cab790489c096f85a71071
Arun Madhavan <arun@ajira.tech>
2020-09-30 12:20:27 +0530

    Arun | Removes extra space

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -2,7 +2,7 @@ module Spree
 	module TaxonsControllerDecorator
 		include Spree::PageTracker
 		def self.prepended(base) 
-			track_actions ([:show], base)
+			track_actions([:show], base)
 		end
 	end
 end
